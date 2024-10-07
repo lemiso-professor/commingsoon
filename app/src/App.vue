4ed1b817047ea068655a9a0fe0a4ed9167e5fc82
lemiso-professor <mathiakipkoech45@gmail.com>
2024-10-07 15:48:34 +0300

    first commit

---
+++ app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="min-h-screen bg-white">
+    <Header />
+    <div class="mt-4">
+      <RouterView />
+    </div>
+    <Cards />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+import Header from "./components/Header.vue";
+import Cards from "./components/Cards.vue";
+</script>
+
+<style scoped></style>
